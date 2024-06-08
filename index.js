1c4255e47e7003970871904b83c4cd2fa615eae6
Saiful Islam <saifulislamsaif797@gmail.com>
2024-06-09 02:07:38 +0600

    Updated cors policy

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://connect-2-study.web.app",
+    "https://connect-2-study.firebaseapp.com",
   ],
   credentials: true,
 }));
